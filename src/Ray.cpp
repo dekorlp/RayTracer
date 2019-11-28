2ffdea631c4683cb73d51c33c3b61d49ea80499b
stdskoeh <dennis.koehler@stud.h-da.de>
2019-11-28 16:17:03 +0100

    implemented Ray class

---
+++ src/Ray.cpp
@@ -0,0 +1,17 @@
+#include "Ray.h"
+
+Ray:: Ray(Vector3 start, Vector3 direction)
+{
+	this->m_Start = start;
+	this->m_Direction = direction;
+}
+
+Vector3 Ray::GetStart()
+{
+	return m_Start;
+}
+
+Vector3 Ray::GetDirection()
+{
+	return m_Direction;
+}
---
+++ src/Ray.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Vector3.h"
+
+class Ray
+{
+private:
+	Vector3 m_Start;
+	Vector3 m_Direction;
+
+public:
+	Ray(Vector3 start, Vector3 direction);
+
+	Vector3 GetStart();
+
+	Vector3 GetDirection();
+	
+};
