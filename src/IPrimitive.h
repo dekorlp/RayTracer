2b6c6fd2069d44d2f69dc25dce3b8c3da6d38ebb
stdskoeh <dennis.koehler@stud.h-da.de>
2019-12-03 15:17:05 +0100

    implemented Sphere base class IPrimitive

---
+++ src/IPrimitive.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Vector3f.h"
+
+class IPrimitive
+{
+public:
+	Vec3f surfaceColor, emissionColor;      /// surface color and emission (light) 
+	float transparency, reflection;         /// surface transparency and reflectivity 
+	Vec3f center;                           /// position of the sphere 
+
+	virtual bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const = 0;
+};
--- src/Sphere.h
+++ src/Sphere.h
@@ -1,15 +1,13 @@
 #pragma once
 
 #include "Vector3f.h"
+#include "IPrimitive.h"
 #include <cmath> 
 
-class Sphere
+class Sphere : public IPrimitive
 {
 public:
-	Vec3f center;                           /// position of the sphere 
 	float radius, radius2;                  /// sphere radius and radius^2 
-	Vec3f surfaceColor, emissionColor;      /// surface color and emission (light) 
-	float transparency, reflection;         /// surface transparency and reflectivity 
 	Sphere(
 		const Vec3f &c,
 		const float &r,
@@ -17,9 +15,13 @@ public:
 		const float &refl = 0,
 		const float &transp = 0,
 		const Vec3f &ec = 0) :
-		center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec),
-		transparency(transp), reflection(refl)
-	{ /* empty */
+		radius(r), radius2(r * r)
+	{ 
+		center = c;
+		surfaceColor = sc;
+		emissionColor = ec;
+		transparency = transp;
+		reflection = refl;
 	}
 
 	bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
